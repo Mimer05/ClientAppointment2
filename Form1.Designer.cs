16438db587e4533584652929bce28702be565463
Ejido, M <ejidomerilyn@gmail.com>
2024-12-10 06:12:55 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ClientAppointment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBxName = new System.Windows.Forms.TextBox();
+            this.TxtBxContactNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CBTime = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RTBNotes = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.RBYes = new System.Windows.Forms.RadioButton();
+            this.RBNo = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // TxtBxName
+            // 
+            this.TxtBxName.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBxName.Location = new System.Drawing.Point(93, 93);
+            this.TxtBxName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtBxName.Name = "TxtBxName";
+            this.TxtBxName.Size = new System.Drawing.Size(438, 27);
+            this.TxtBxName.TabIndex = 0;
+            // 
+            // TxtBxContactNumber
+            // 
+            this.TxtBxContactNumber.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBxContactNumber.Location = new System.Drawing.Point(93, 154);
+            this.TxtBxContactNumber.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtBxContactNumber.Name = "TxtBxContactNumber";
+            this.TxtBxContactNumber.Size = new System.Drawing.Size(438, 27);
+            this.TxtBxContactNumber.TabIndex = 3;
+            this.TxtBxContactNumber.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 130);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contact number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 195);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Appointment Schedule:";
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.submitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.submitBtn.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.submitBtn.Location = new System.Drawing.Point(119, 533);
+            this.submitBtn.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(361, 32);
+            this.submitBtn.TabIndex = 8;
+            this.submitBtn.Text = "Send the Appointment Schedule";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(105, 223);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(441, 27);
+            this.dateTimePicker.TabIndex = 9;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // CBTime
+            // 
+            this.CBTime.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBTime.FormattingEnabled = true;
+            this.CBTime.Items.AddRange(new object[] {
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM",
+            "5:00 PM",
+            "6:00 PM",
+            "7:00 PM",
+            "8:00 PM"});
+            this.CBTime.Location = new System.Drawing.Point(105, 260);
+            this.CBTime.Name = "CBTime";
+            this.CBTime.Size = new System.Drawing.Size(441, 28);
+            this.CBTime.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 228);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Date:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 268);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Time:";
+            // 
+            // RTBNotes
+            // 
+            this.RTBNotes.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTBNotes.Location = new System.Drawing.Point(92, 329);
+            this.RTBNotes.Name = "RTBNotes";
+            this.RTBNotes.Size = new System.Drawing.Size(439, 144);
+            this.RTBNotes.TabIndex = 15;
+            this.RTBNotes.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(30, 306);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(209, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Appointment details:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(64, 9);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(498, 42);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Appointment Schedule";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label8.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(30, 476);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(579, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Would you agree to reschedule the appointment, if needed?";
+            // 
+            // RBYes
+            // 
+            this.RBYes.AutoSize = true;
+            this.RBYes.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBYes.Location = new System.Drawing.Point(189, 502);
+            this.RBYes.Name = "RBYes";
+            this.RBYes.Size = new System.Drawing.Size(60, 24);
+            this.RBYes.TabIndex = 19;
+            this.RBYes.TabStop = true;
+            this.RBYes.Text = "Yes";
+            this.RBYes.UseVisualStyleBackColor = true;
+            // 
+            // RBNo
+            // 
+            this.RBNo.AutoSize = true;
+            this.RBNo.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBNo.Location = new System.Drawing.Point(346, 502);
+            this.RBNo.Name = "RBNo";
+            this.RBNo.Size = new System.Drawing.Size(50, 24);
+            this.RBNo.TabIndex = 20;
+            this.RBNo.TabStop = true;
+            this.RBNo.Text = "No";
+            this.RBNo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 578);
+            this.Controls.Add(this.RBNo);
+            this.Controls.Add(this.RBYes);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.RTBNotes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CBTime);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBxContactNumber);
+            this.Controls.Add(this.TxtBxName);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Appoint Schedule";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBxName;
+        private System.Windows.Forms.TextBox TxtBxContactNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox CBTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox RTBNotes;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton RBYes;
+        private System.Windows.Forms.RadioButton RBNo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ClientAppointment
+{
+    public partial class Form1 : Form
+    {
+        // Connection string to the database
+        private const string ConnectionString = "Data Source=LAPTOP-R6NA64TT\\SQLEXPRESS;Initial Catalog=\"Appointment Scheduling\";Integrated Security=True;Encrypt=False";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            string query = @"INSERT INTO Appointments (Name, [Contact Number], Date, Time, [Appointment Details], Reschedule) 
+                             VALUES (@Name, @ContactNumber, @Date, @Time, @AppointmentDetails, @Reschedule)";
+
+            // Open connection and execute the SQL query
+            ExecuteQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@Name", TxtBxName.Text.Trim());
+                cmd.Parameters.AddWithValue("@ContactNumber", TxtBxContactNumber.Text.Trim());
+                cmd.Parameters.AddWithValue("@Date", dateTimePicker.Value);
+                cmd.Parameters.AddWithValue("@Time", GetValidTime());
+                cmd.Parameters.AddWithValue("@AppointmentDetails", string.IsNullOrEmpty(RTBNotes.Text) ? DBNull.Value : (object)RTBNotes.Text.Trim());
+                cmd.Parameters.AddWithValue("@Reschedule", RBYes.Checked ? 1 : 0);
+            });
+            // Ask the user if they want a PDF copy
+            var result = MessageBox.Show("Do you want to generate a PDF of the appointment?",
+                                         "Generate PDF",
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            // If the user clicks 'Yes', generate the PDF
+            if (result == DialogResult.Yes)
+            {
+                GeneratePdf();
+                MessageBox.Show("Thank you!");
+            }
+            else
+            {
+                MessageBox.Show("Thank you!");
+            }
+        }
+        // This function executes a given query with the provided parameters
+        private void ExecuteQuery(string query, Action<SqlCommand> addParameters)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        addParameters(cmd);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show(rowsAffected > 0 ? "Operation successful." : "Operation failed.");
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"SQL Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+        // Helper function to ensure the time format is valid
+        private string GetValidTime()
+        {
+            if (DateTime.TryParse(CBTime.Text.Trim(), out DateTime parsedTime))
+                return parsedTime.ToString("HH:mm:ss");
+            MessageBox.Show("Invalid time format. Please use HH:mm:ss.");
+            return string.Empty;
+        }
+        // Function to generate a PDF with appointment details
+        private void GeneratePdf()
+        {
+            string filePath = "C:\\Users\\Meri\\Downloads\\appointment_details.pdf";
+            using (Document doc = new Document())
+            {
+                PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+                doc.Open();
+                doc.Add(new Paragraph("Appointment Details"));
+                doc.Add(new Paragraph($"Name: {TxtBxName.Text.Trim()}"));
+                doc.Add(new Paragraph($"Contact: {TxtBxContactNumber.Text.Trim()}"));
+                doc.Add(new Paragraph($"Date: {dateTimePicker.Value.ToShortDateString()}"));
+                doc.Add(new Paragraph($"Time: {CBTime.Text.Trim()}"));
+                doc.Add(new Paragraph($"Details: {RTBNotes.Text.Trim()}"));
+                doc.Add(new Paragraph($"Reschedule: {(RBYes.Checked ? "Yes" : "No")}"));
+            }
+            MessageBox.Show("PDF generated successfully!");
+        }
+        // Function to load all appointments from the database
+        private void LoadAppointments()
+        {
+            string query = "SELECT * FROM Appointments";
+            ExecuteQuery(query, cmd => { });
+        }
+        /* Function to update an appointment in the database
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            string query = @"UPDATE Appointments 
+                             SET [Appointment Details] = @NewDetails, Reschedule = @Reschedule
+                             WHERE Name = @Name AND [Contact Number] = @ContactNumber";
+            ExecuteQuery(query, cmd =>
+            {
+                cmd.Parameters.AddWithValue("@Name", TxtBxName.Text.Trim());
+                cmd.Parameters.AddWithValue("@ContactNumber", TxtBxContactNumber.Text.Trim());
+                cmd.Parameters.AddWithValue("@NewDetails", RTBNotes.Text.Trim());
+                cmd.Parameters.AddWithValue("@Reschedule", RBYes.Checked ? 1 : 0);
+            });
+        }
+
+        // Function to delete an appointment from the database
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            string query = @"DELETE FROM Appointments WHERE Name = @Name AND [Contact Number] = @ContactNumber";
+            ExecuteQuery(query, cmd =>
+            {
+                cmd.Parameters.AddWithValue("@Name", TxtBxName.Text.Trim());
+                cmd.Parameters.AddWithValue("@ContactNumber", TxtBxContactNumber.Text.Trim());
+            });
+        }*/
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
